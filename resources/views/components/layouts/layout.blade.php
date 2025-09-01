e4e539e8bb24b17b0bc427029f04226d7b5a94a7
PabloLahoz <lahoz.pablo.smr@gmail.com>
2025-09-01 09:20:27 +0200

    Añadido icono

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -7,6 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$titulo ?? ""}}</title>
+    <link rel="icon" href="{{asset('img/logo.png')}}" type="image/x-icon">
     @vite (["resources/css/app.css","resources/js/app.js"])
     @livewireStyles
 </head>
